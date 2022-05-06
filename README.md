8f02a3a1cce228082285d16c10f0c4137c51a63c
laughingbiscuit <sean@laughingbiscuit.com>
2022-05-06 11:47:54 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rapid-clickable-wireframes
+
 Building clickable lo-fi wireframes really quickly using plantuml, envsubst and svg links. Great for workshops! 
+
+## Description
+
+Scrappy shell script to orchestrate the following...
+
+- Use a template UML file containing headers and footers for all pages and inject page content and title based on normalised filename
+- Generate SVG with plantuml
+- Add links based on normalised file names. e.g. all instances of "Sample Page" will be linked to SamplePage.html
+- Create html pages containing svg images
+
+Deploy the result from ./target/static and let your team click around!
+
+## Prerequisites
+
+- Java
+- plantuml + graphviz
+- POSIX compliant shell
+- `envsubst` (from `gettext`)
+- `xmllint` and `xmlstarlet` (formatting and tidying)
+
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -e
+set -x
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+rm -rf target
+mkdir -p target/inter
+mkdir -p target/static
+
+for PAGE in $(find . | grep '\.uml$'); do
+  # embed title and content
+  FORMATTED_TITLE=$(echo $PAGE | sed 's/\.uml//' | sed 's/\([a-z]\)\([A-Z]\)/\1 \2/g' | sed 's/^\.\///')
+  TITLE="$FORMATTED_TITLE" CONTENT="$(cat $PAGE)" \
+    envsubst < $SCRIPTPATH/config/template.puml > $SCRIPTPATH/target/inter/$PAGE
+
+  # convert to svg
+  (cd $SCRIPTPATH/target/inter && java -jar /opt/plantuml.jar -tsvg $PAGE)
+
+  # format xml
+  SVG_PAGE="$(echo $PAGE | sed 's/\.uml$/.svg/')"
+  (cd $SCRIPTPATH/target/inter && xmllint --format $SVG_PAGE > $SVG_PAGE.new && mv $SVG_PAGE.new $SVG_PAGE)
+  # remove comments
+  (cd $SCRIPTPATH/target/inter && xmlstarlet ed -d '//comment()' $SVG_PAGE > $SVG_PAGE.new && mv $SVG_PAGE.new $SVG_PAGE)
+done
+
+for PAGE in $(cd $SCRIPTPATH/target/inter && find . | grep '\.svg$'); do
+  # add links
+  for LINK_TARGET in $(cd $SCRIPTPATH/target/inter && find . | grep '\.svg$'); do
+    LINK_TEXT=$(echo $LINK_TARGET | sed 's/\.svg//' | sed 's/\([a-z]\)\([A-Z]\)/\1 \2/g' | sed 's/^\.\///')
+    sed -i "s/$LINK_TEXT/<a target=\"_top\" href=\"$(basename $LINK_TARGET | sed 's/svg$/html/')\">$LINK_TEXT<\/a>/" $SCRIPTPATH/target/inter/$PAGE
+  done
+
+  # make static_html page
+  cp $SCRIPTPATH/target/inter/$PAGE $SCRIPTPATH/target/static/
+  HTML_NAME="$(echo $PAGE | sed "s/svg$/html/")" 
+  IMG_SRC=$PAGE \
+    envsubst < $SCRIPTPATH/config/template.html > $SCRIPTPATH/target/static/$HTML_NAME
+  
+done
